ebe83f5b1db4e4df6121b35df58e18b22227d7f8
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-05-04 20:27:42 +0000

    [Feature] The request library is used to get data of an API. - The data is parsed to JSON. new file:   0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""  """
+if __name__ == "__main__":
+    import requests as req
+    from sys import argv
+
+    emplo_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users"
+    emplo_info = req.get(url + "/{}".format(emplo_id)).json()
+    emplo_tasks = req.get(url + "/{}/todos".format(emplo_id)).json()
+    list_tasks = []
+    for task in emplo_tasks:
+        if task["completed"] is True:
+            list_tasks.append(task["title"])
+
+    print("Employee " + emplo_info["name"] + " is done with tasks ({}/{}):"
+          .format(len(list_tasks), len(emplo_tasks)))
+    for task in list_tasks:
+        print("\t{}".format(task))
