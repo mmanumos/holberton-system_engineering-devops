780d149970cf8e0fe59ff54e1591582c93f3703f
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-05-05 01:18:30 +0000

    [Feature] Exporting Json data to a CSV file. new file:   1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""  """
+if __name__ == "__main__":
+    import requests as req
+    from sys import argv
+    import csv
+
+    emplo_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users"
+    emplo_info = req.get(url + "/{}".format(emplo_id)).json()
+    emplo_tasks = req.get(url + "/{}/todos".format(emplo_id)).json()
+
+    with open(emplo_id + ".csv", "w", newline="") as csvfile:
+        file = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in emplo_tasks:
+            file.writerow([emplo_id,
+                           emplo_info.get("name"),
+                           task.get("completed"),
+                           task.get("title")])
