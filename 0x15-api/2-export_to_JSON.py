ad05c70fa53ec206aed52eacf203df7da9b5d1e9
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-05-05 01:39:42 +0000

    [Feature] Python objects are exported to Json file. new file:   2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""  """
+if __name__ == "__main__":
+    import requests as req
+    from sys import argv
+    import json
+
+    emplo_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users"
+    emplo_info = req.get(url + "/{}".format(emplo_id)).json()
+    emplo_tasks = req.get(url + "/{}/todos".format(emplo_id)).json()
+
+    list_task = []
+    for task in emplo_tasks:
+        dict_task = {}
+        dict_task["task"] = task.get("title")
+        dict_task["completed"] = task.get("completed")
+        dict_task["completed"] = task.get("completed")
+        list_task.append(dict_task)
+
+    dict_task_emplo = {emplo_id: list_task}
+
+    with open(emplo_id + ".json", "w") as file:
+        json.dump(dict_task_emplo, file)
