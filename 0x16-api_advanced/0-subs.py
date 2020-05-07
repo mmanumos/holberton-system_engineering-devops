ada203bf6c02965dc292c7bec5a952e4f9015840
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-05-07 23:54:27 +0000

    [Fix] Removing validation with if. modified:   0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,9 +4,6 @@ import requests as re
 
 def number_of_subscribers(subreddit):
     """ Return number of subscribers """
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
     headers = {'User-agent': 'me'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     data = re.get(url, headers=headers, allow_redirects=False).json()
