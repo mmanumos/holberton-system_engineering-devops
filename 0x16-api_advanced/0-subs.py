a28b0b6d174f9a2f06025a6f28f61fc2d191c85f
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-05-07 23:33:55 +0000

    [Fix] Adding redirects -> False. modified:   0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
 
     headers = {'User-agent': 'me'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    data = re.get(url, headers=headers).json()
+    data = re.get(url, headers=headers, allow_redirects=False).json()
     try:
         return (data.get('data').get('subscribers'))
     except:
